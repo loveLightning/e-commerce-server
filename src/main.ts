b3a5120b5ebe6ac01616208254742d1667da4ccf
Evgeniy <74300821+zaycevnet20@users.noreply.github.com>
2023-05-28 22:14:02 +0400

    change header

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,19 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  })
+  const app = await NestFactory.create(AppModule)
 
-  // app.enableCors({
-  //   origin: [
-  //     process.env.CLIENT_URL,
-  //     process.env.ADMIN_URL,
-  //     'https://videohosting-next-js-nest-js-admin-86btbkj8n-zaycevnet20.vercel.app',
-  //     'https://videohosting-next-js-nest-js-client-j3hpnrztb-zaycevnet20.vercel.app',
-  //   ],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  //   // allowedHeaders: ['authorization', 'content-type'],
-  // })
+  app.enableCors({
+    origin: [
+      process.env.CLIENT_URL,
+      process.env.ADMIN_URL,
+      'https://videohosting-next-js-nest-js-admin-86btbkj8n-zaycevnet20.vercel.app',
+      'https://videohosting-next-js-nest-js-client-j3hpnrztb-zaycevnet20.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: ['authorization', 'content-type'],
+  })
 
   app.useGlobalPipes(
     new ValidationPipe({
